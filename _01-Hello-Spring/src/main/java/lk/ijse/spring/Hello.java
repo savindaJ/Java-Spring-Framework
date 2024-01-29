d937d75312cf35faa9213bf60866a5f91baee28d
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-01-29 09:52:12 +0530

    add demo file

---
+++ _01-Hello-Spring/src/main/java/lk/ijse/spring/Hello.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring;
+
+/**
+ * @author : savindaJ
+ * @date : 2024-01-29
+ * @since : 0.1.0
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Spring !");
+    }
+}
