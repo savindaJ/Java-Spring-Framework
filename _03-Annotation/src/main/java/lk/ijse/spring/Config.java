e5cf5983bcce574d27e6ce375ea579c4b0ca1b4c
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-02 15:43:08 +0530

    add componenets

--- _03-Annotation/src/main/java/lk/ijse/spring/Config.java
+++ _03-Annotation/src/main/java/lk/ijse/spring/Config.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
  * @date : 2024-02-02
  * @since : 0.1.0
  **/
-@Component
+@Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.bean") // search components in root but lk.ijse.spring.bean -> this path scan one this path
 public class Config {
     public Config() {
