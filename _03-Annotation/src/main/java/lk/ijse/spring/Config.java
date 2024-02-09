6c46336e7db16f336b3b5e59575bc1710efc8383
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-09 10:50:34 +0530

    add componenets

--- _03-Annotation/src/main/java/lk/ijse/spring/Config.java
+++ _03-Annotation/src/main/java/lk/ijse/spring/Config.java
@@ -5,6 +5,9 @@ import lk.ijse.spring.bean.BeanTwo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @author : savindaJ
  * @date : 2024-02-02
@@ -12,8 +15,17 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 @ComponentScan(/*basePackages = "lk.ijse.spring.bean" ,*/ basePackageClasses = BeanTwo.class) // search components in root but lk.ijse.spring.bean -> this path scan one this path
+// can set many classes by using = {BeanOne.class,BeanTwo.class}
 public class Config {
-    public Config() {
+    public Config() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         System.out.println("config");
+        Class<?> test = Class.forName("lk.ijse.spring.Test");
+
+        Constructor<?> constructor = test.getConstructor();
+
+        Test o = (Test) constructor.newInstance();
+
+        System.out.println("test class class name ::>"+o.get());
+
     }
 }
---
+++ _03-Annotation/src/main/java/lk/ijse/spring/Test.java
@@ -0,0 +1,12 @@
+package lk.ijse.spring;
+
+/**
+ * @author : savindaJ
+ * @date : 2024-02-09
+ * @since : 0.1.0
+ **/
+public final class Test {
+    public Test get(){
+        return this;
+    }
+}
