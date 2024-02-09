19338a93cdf63953553eae824a7270e568a7457f
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-09 14:03:47 +0530

    add life cycle

---
+++ _06-Bean-Life-Cycle/src/main/java/lk/ijse/spring/bean/BeanOne.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : savindaJ
+ * @date : 2024-02-09
+ * @since : 0.1.0
+ **/
+@Component
+public class BeanOne {
+    public BeanOne() {
+        System.out.println("bean one !");
+    }
+}
