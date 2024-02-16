869f6e8e5ce2ffd8b908c73db2dfcb1004f78a33
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-16 16:19:00 +0530

    bean injections

--- _08-RunTime-Value-nject/src/main/java/lk/ijse/spring/Bean.java
+++ _08-RunTime-Value-nject/src/main/java/lk/ijse/spring/Bean.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
