d2076b83a1aa617b5ad9aa00221d3fb10ebdbe7a
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-22 12:13:19 +0530

    add configuration oparations

--- _11-Spring-Configuration/src/main/java/lk/ijse/spring/config/AppConfigRoot.java
+++ _11-Spring-Configuration/src/main/java/lk/ijse/spring/config/AppConfigRoot.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.ImportResource;
 @Configuration
 @ComponentScan("lk.ijse.spring.bean")
 @ImportResource("classpath:spring.xml")
+@Import({AppConfigDAO.class , AppConfigBO.class}) // sey register my other config classes
 public class AppConfigRoot {
     public AppConfigRoot() {
         System.out.println("AppConfigTwo Instantiated");
