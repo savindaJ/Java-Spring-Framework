342a60ec2babcf639bda958bb2591ae0a8e89ac6
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-22 14:07:09 +0530

    add runtime value injection diff tyoes and primitive

--- _12-RunTime-Value-Injection/src/main/java/lk/ijse/spring/bean/BeanOne.java
+++ _12-RunTime-Value-Injection/src/main/java/lk/ijse/spring/bean/BeanOne.java
@@ -4,21 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author : savindaJ
  * @date : 2024-02-22
  * @since : 0.1.0
  **/
 @Component
-public class BeanOne implements InjectNic{
+public class BeanOne implements InjectNic {
 
     @Value("Colombo, Sri Lanka")
     String address;
 
-    public BeanOne(@Value("Jayasekara") String name, @Value("20") int age) {
+    public BeanOne(
+            @Value("Jayasekara") String name,
+            @Value("20") int age,
+            @Value("25000.90") double salary,
+            @Value("true") boolean isEmployed,
+            @Value("10,20,30,40") int[] marks
+    ) {
         System.out.println("BeanOne Constructor !");
         System.out.println("Name : " + name);
         System.out.println("Age : " + age);
+        System.out.println("Salary : " + salary);
+        System.out.println("Employed : " + isEmployed);
+        System.out.println("Marks : " + Arrays.toString(marks));
         System.out.println("Address : " + address); // prints null , because the address is not after the property set
     }
 
