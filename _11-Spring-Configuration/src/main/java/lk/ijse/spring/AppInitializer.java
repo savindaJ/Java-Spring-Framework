68a338c2db3af9c024e179cbafcbc8b03f364802
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-02-22 11:22:43 +0530

    add configuration modeul

---
+++ _11-Spring-Configuration/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.AppConfigOne;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * @author : savindaJ
+ * @date : 2024-02-22
+ * @since : 0.1.0
+ **/
+public class AppInitializer {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+        ctx.register(AppConfigOne.class);
+        ctx.refresh();
+        ctx.registerShutdownHook();
+    }
+}
