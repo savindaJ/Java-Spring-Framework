2f3cf5eb1a6f875905ec803d4382d7451902d9dd
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-03-07 09:32:01 +0530

    add crud

--- _17-Spring-web-Crud/src/main/java/lk/ijse/spring/crud/config/WebAppConfig.java
+++ _17-Spring-web-Crud/src/main/java/lk/ijse/spring/crud/config/WebAppConfig.java
@@ -1,9 +1,16 @@
 package lk.ijse.spring.crud.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author : savindaJ
@@ -15,4 +22,15 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "lk.ijse.spring.crud.controller")
 public class WebAppConfig {
+    @Bean
+    public Connection connection(
+            @Value("${spring.web.user}") String user,
+            @Value("${spring.web.password}") String password,
+            @Value("${spring.web.url}") String url,
+            @Value("${spring.web.driver}") String driver
+    ) throws ClassNotFoundException, SQLException {
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(url, user, password);
+        return connection;
+    }
 }
