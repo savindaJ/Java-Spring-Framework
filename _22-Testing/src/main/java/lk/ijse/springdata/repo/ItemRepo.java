bab419793754ab7e02de805c3d638467efb3b938
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-04-04 12:38:37 +0530

    query by using jpa statement

--- _22-Testing/src/main/java/lk/ijse/springdata/repo/ItemRepo.java
+++ _22-Testing/src/main/java/lk/ijse/springdata/repo/ItemRepo.java
@@ -2,6 +2,10 @@ package lk.ijse.springdata.repo;
 
 import lk.ijse.springdata.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 /**
  * @author : savindaJ
@@ -9,4 +13,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since : 0.1.0
  **/
 public interface ItemRepo extends JpaRepository<Item,String> {
+    @Query(value = "SELECT * FROM Item WHERE description LIKE %?1%", nativeQuery = true)
+    List<Item> findItemByName(String name);
+    @Query(value = "select * from Item order by code desc limit 1", nativeQuery = true)
+    Item getLastItem();
+    List<Item> findItemByCodeEndingWithOrderByCodeDesc(String code);
+    Item searchByCodeAndDescription(String code, String description);
 }
--- _22-Testing/src/test/java/lk/ijse/springdata/repo/ItemRepoTest.java
+++ _22-Testing/src/test/java/lk/ijse/springdata/repo/ItemRepoTest.java
@@ -1,6 +1,14 @@
 package lk.ijse.springdata.repo;
 
+import lk.ijse.springdata.config.WebRootConfig;
+import lk.ijse.springdata.entity.Item;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,11 +17,41 @@ import static org.junit.jupiter.api.Assertions.*;
  * @date : 2024-04-04
  * @since : 0.1.0
  **/
+@Transactional
+@SpringJUnitConfig(classes = {WebRootConfig.class})
+@WebAppConfiguration
 class ItemRepoTest {
 
+    @Autowired
+    ItemRepo itemRepo;
+
     @Test
-    void testSave() {
-        fail("Not yet implemented");
+    void testSaveItem() {
+        Item save = itemRepo.save(new Item("ITM001", "Item 1", 100.3, 10));
+        assertNotNull(save);
     }
 
+    @Test
+    void testUpdateItem() {
+        List<Item> itemByName = itemRepo.findItemByName("M");
+        System.out.println(itemByName.toString());
+    }
+
+    @Test
+    void testFindItemByName() {
+        Item lastItem = itemRepo.getLastItem();
+        System.out.println(lastItem);
+    }
+
+    @Test
+    void testFindItemByCodeEndingWithOrderByCodeDesc() {
+        List<Item> item = itemRepo.findItemByCodeEndingWithOrderByCodeDesc("3");
+        System.out.println(item.toString());
+    }
+
+    @Test
+    void testSearchByCodeAndDescription() {
+        Item item = itemRepo.searchByCodeAndDescription("ITM-360948a2", "Raththi");
+        System.out.println(item);
+    }
 }
