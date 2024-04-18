c92d715bb4240662bc213e7753d0b64246d0df48
savinda jayasekara <savindajayasekara@savindas-MacBook-Air.local>
2024-04-18 11:26:20 +0530

    add basic athintication

---
+++ _24-Spring-Security/src/main/java/lk/ijse/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package lk.ijse.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author : savindaJ
+ * @date : 2024-04-18
+ * @since : 0.1.0
+ **/
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable) // Disable CSRF use method reference
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()); // Enable HTTP Basic Authentication
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder().username("savinda").password("80221474").roles("USER").build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
